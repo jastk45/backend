c229a7d16b555d79a25c1533ae51aeb6824d6853
jastk45 <jastk45@gmail.com>
2023-07-17 09:57:05 -0500

    mongo test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from db.models.question import Form
+from db.client import client
 
 app = FastAPI()
 
@@ -12,3 +14,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/form")
+def create_form(form: Form):
+    # Aquí puedes guardar el formulario en tu base de datos o realizar otras operaciones
+    client.jastk45.forms.insert_one({'x': 1})
+    return {"message": "Form created successfully"}
